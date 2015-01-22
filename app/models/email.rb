201868f1c4e8f575447404b63098fa232d513cf9
Peter He <hexinpeter@gmail.com>
2015-01-23 04:05:17 +1100

    basic jQuery form validatoin added

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,4 @@
 class Email < ActiveRecord::Base
+  validates :subject, :recipient, presence: true
+  validates :recipient, format: { with: %r{\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z}i }
 end
